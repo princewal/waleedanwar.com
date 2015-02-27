1617a35e5ee34a1a97d360bcf9ead90b64efaea6
princewal <princewal@hotmail.com>
2015-02-27 14:47:20 -0500

    added scroll functionality

--- V3/js/main.js
+++ V3/js/main.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function(){
+	$("#btnMain").on("click", function(){
+	/*
+	 	$('.content-wrap').animate({
+        	scrollTop: $("#aboutMe").offset().top
+	    }, 2000);
+	    return false;*/
+	    $('.content-wrap').scrollTo('#aboutMe', 1500);
+	     return false;
+
+	});
+
+});
